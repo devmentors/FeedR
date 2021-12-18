52e8294a2ea31537f6d1125d3a929fddbf57d81e
spetz <piotr.gankiewicz@gmail.com>
2021-12-18 14:30:58 +0100

    Episode 3 - YARP API Gateway

--- src/Gateway/FeedR.Gateway/Program.cs
+++ src/Gateway/FeedR.Gateway/Program.cs
@@ -1,8 +1,15 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("yarp"));
+    
 var app = builder.Build();
 
 app.MapGet("/", () => "FeedR Gateway");
+app.MapReverseProxy();
 
 app.Run();
