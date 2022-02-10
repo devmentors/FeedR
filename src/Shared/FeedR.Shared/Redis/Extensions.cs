68a14a490e46ef515cbb93327fbfa4267b9ed65a
Kia Panahi Rad <kia.panahirad@gmail.com>
2022-02-10 16:20:06 +0330

    Refactor AddRedis Extension method

--- src/Shared/FeedR.Shared/Redis/Extensions.cs
+++ src/Shared/FeedR.Shared/Redis/Extensions.cs
@@ -6,16 +6,34 @@ namespace FeedR.Shared.Redis;
 
 public static class Extensions
 {
-    public static IServiceCollection AddRedis(this IServiceCollection services, IConfiguration configuration)
+    /// <summary>
+    /// Adds Redis to <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <remarks>
+    /// Uses the lower-level <see cref="ServiceDescriptor"/> APIs to acquire an instance of <see cref="IConfiguration"/>
+    /// without the need of passing it in explicitly
+    /// </remarks>
+    /// <returns>The same <see cref="IServiceCollection"/> instance.</returns>
+    public static IServiceCollection AddRedis(this IServiceCollection services)
     {
-        var section = configuration.GetRequiredSection("redis");
-        var options = new RedisOptions();
-        section.Bind(options);
-        services.Configure<RedisOptions>(section);
-        services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(options.ConnectionString));
-        
+        var serviceDescriptor = ServiceDescriptor.Singleton<IConnectionMultiplexer>(sp =>
+        {
+            var options = sp.GetRequiredService<IConfiguration>().GetRequiredSection("redis").Get<RedisOptions>();
+            return ConnectionMultiplexer.Connect(options.ConnectionString);
+        });
+
+        services.Add(serviceDescriptor);
+
         return services;
     }
-    
-    
+
+    /// <summary>
+    /// Adds Redis to <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <returns>The same <see cref="IServiceCollection"/> instance.</returns>
+    [Obsolete("Use the overload that does not take the IConfiguration explicitely")]
+    public static IServiceCollection AddRedis(this IServiceCollection services, IConfiguration configuration)
+    {
+        return AddRedis(services);
+    }
 }
